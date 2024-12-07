e3df12e58c8c561c95c62cc8c3b125012cf70187
surajbhoji1208 <60956130+surajbhoji1208@users.noreply.github.com>
2024-12-07 13:20:46 +0530

    3.5 configure redux store add and remove

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import { Body } from './components/Body';
+import appStore from './utils/appStroe';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-   <Body></Body>
+    <Provider store={appStore}>
+      <Body></Body>
+    </Provider>
   );
 }
 
