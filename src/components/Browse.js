496ee1d3c69af6c7a61857cea51bcc51017859cb
surajbhoji1208 <60956130+surajbhoji1208@users.noreply.github.com>
2024-12-07 23:19:14 +0530

    15 0.55 created new reduct strore for moview

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,13 +1,19 @@
 import React, { useEffect } from 'react'
 import { Header } from './Header'
 import { API_OPTION } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import { addNowPlayingMovies } from '../utils/moviesSlice'
 
 export const Browse = () => {
+
+  /**fetch data from api and strore the data in redux */
+  const dispatch = useDispatch()
   const getNowPlayingMovies = async ()=>
   {
     const data = await fetch( 'https://api.themoviedb.org/3/movie/now_playing?page=1',API_OPTION)
     const json = await data.json()
     console.log(json.results);
+    dispatch(addNowPlayingMovies)
   }
 
   useEffect(()=>{
