cbc758ad7c856a7d45e9381906b8a7f9fd8db69e
surajbhoji1208 <60956130+surajbhoji1208@users.noreply.github.com>
2024-12-07 23:02:30 +0530

    15 0.50 create api call for movie list with TMDB

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,8 @@
 export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+export const API_OPTION = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjlkNWRjYmM5ZTIzMTdmMDdkNTc4YjdjMmMwN2Q2ZSIsIm5iZiI6MTczMzU5MTQ2Ny40Mywic3ViIjoiNjc1NDgxYWI4MDJiYWQxNjA5MWFlMDlkIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.xCj2kLLtY7r0Ow4I3sUosmpRqS1Fw__ivMH5QEp_0mQ'
+    }
+  };
