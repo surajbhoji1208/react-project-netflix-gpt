34d0cc88a296d74c4d955658d43aeb86131355dc
surajbhoji1208 <60956130+surajbhoji1208@users.noreply.github.com>
2024-12-08 10:59:57 +0530

    15 2.2 added trailler details to store

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,11 +5,9 @@ import VideoBackground from './VideoBackground'
 
 function MainContainer() {
     const movie = useSelector((store)=>store.movies?.nowPlayingMovies)
-    console.log("8:",movie);
     if(!movie) return
 
     const firstMovie = movie.results[0]
-    console.log("firstMovie=>",firstMovie);
     
     const {original_title,overview} = firstMovie
   return (
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,22 +1,31 @@
 import React, { useEffect } from 'react'
 import { API_OPTION } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addTrailerVideo } from '../utils/moviesSlice'
 
 function VideoBackground({movieId}) {
-
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  const dispatch = useDispatch()
   const getMovieVideos =async ()=>{
     const data =await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`,API_OPTION)
     const json = await data.json();
-    console.log("getMovieVideos=>",json);
     const filterData = json.results?.filter((video)=>video.type =='Trailer')
     const trailer = filterData?.length?filterData[0] :json.results[0]
+    dispatch(addTrailerVideo(trailer))
 
   }
   useEffect(()=>{
     getMovieVideos()
   },[])
   return (
-    <div>VedioBackgrouni</div>
-  )
+    <div>
+      <iframe
+        src={"https://www.youtube.com/embed/FKBN1qAzW3s"+trailerVideo.key}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
 }
 
 export default VideoBackground
